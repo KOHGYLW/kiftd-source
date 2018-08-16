218558784fc721e26c02d0edf27c1613b63f783e
pzh244658240 <244658240@qq.com>
2018-08-16 16:37:29 +0800

    增加跨域访问的控制，只要在提交参数中增加cross=accept就支持跨域访问

--- src/main/java/kohgylw/kiftd/server/filter/MastLoginFilter.java
+++ src/main/java/kohgylw/kiftd/server/filter/MastLoginFilter.java
@@ -4,6 +4,10 @@ import javax.servlet.annotation.*;
 import javax.servlet.*;
 import kohgylw.kiftd.server.util.*;
 import javax.servlet.http.*;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 import java.io.*;
 
 @WebFilter
@@ -19,6 +23,8 @@ public class MastLoginFilter implements Filter
         final HttpServletResponse hsr = (HttpServletResponse)response;
         final String url = hsq.getServletPath();
         final HttpSession session = hsq.getSession();
+        //检测跨域访问
+        checkCross();
         if (url.equals("//prv/login.html") || url.equals("/prv/login.html")) {
             if (s) {
                 final String account = (String)session.getAttribute("ACCOUNT");
@@ -79,7 +85,25 @@ public class MastLoginFilter implements Filter
             chain.doFilter(request, response);
         }
     }
-    
+    /**
+     * @author pengzhonghui
+     * 检测当前是否支持将跨域访问开放
+     */
+    public void checkCross() {
+    	HttpServletResponse resp = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+    	final String cross = request.getParameter("cross");
+    	if(cross.equals("accept")){
+			resp.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+			resp.setHeader("Access-Control-Allow-Credentials", "true");
+			resp.setHeader("P3P", "CP=CAO PSA OUR");
+	        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+	        	resp.addHeader("Access-Control-Allow-Methods", "POST,GET,TRACE,OPTIONS");
+	        	resp.addHeader("Access-Control-Allow-Headers", "Content-Type,Origin,Accept");
+	        	resp.addHeader("Access-Control-Max-Age", "120");
+	        }
+		}
+    }
     public void destroy() {
     }
 }
