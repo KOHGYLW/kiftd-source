1046f8a29265026b2c021188c3f150a203e752cc
kohgylw <kohgylw@163.com>
2018-09-09 16:55:01 +0800

    =完成部分功能，向v1.0.5版本推进。

--- src/main/java/kohgylw/kiftd/mc/MC.java
+++ src/main/java/kohgylw/kiftd/mc/MC.java
@@ -1,13 +1,15 @@
 package kohgylw.kiftd.mc;
 
-public class MC
-{
-    public static void main(final String[] args) {
-        if (args == null || args.length == 0) {
-            UIRunner.build();
-        }
-        else {
-            ConsoleRunner.build(args);
-        }
-    }
+public class MC {
+	public static void main(final String[] args) {
+		if (args == null || args.length == 0) {
+			try {
+				UIRunner.build();
+			} catch (Exception e) {
+				System.out.println("错误！无法以图形界面模式启动kiftd，您的操作系统可能不支持图形界面。您可以尝试使用命令模式“-console”来启动kiftd并开始使用。");
+			}
+		} else {
+			ConsoleRunner.build(args);
+		}
+	}
 }
