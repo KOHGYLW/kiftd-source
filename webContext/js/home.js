b989d610b07e2bf64d1b793238636f5b63541ab1
kohgylw <kohgylw@163.com>
2018-11-08 10:43:03 +0800

    update to v1.0.8 再次更新了图片分页加载，这回应该没什么问题了……

--- webContext/js/home.js
+++ webContext/js/home.js
@@ -1351,9 +1351,9 @@ function createViewListByPage() {
 		}
 		for(var i = 0; i < viewerPageSize && i < (pvl.pictureViewList.length - (viewerPageIndex - 1) * viewerPageSize); i++) {
 			if(pvl.pictureViewList[i].filePath.startsWith("homeController")){
-				$(images).append("<li><img src='" + pvl.pictureViewList[startIndex+i].filePath + "' alt='" + pvl.pictureViewList[i].fileName + "' /></li>");
+				$(images).append("<li><img src='" + pvl.pictureViewList[startIndex+i].filePath + "' alt='" + pvl.pictureViewList[startIndex+i].fileName + "' /></li>");
 			}else{
-				$(images).append("<li><img src='fileblocks/" + pvl.pictureViewList[startIndex+i].filePath + "' alt='" + pvl.pictureViewList[i].fileName + "' /></li>");
+				$(images).append("<li><img src='fileblocks/" + pvl.pictureViewList[startIndex+i].filePath + "' alt='" + pvl.pictureViewList[startIndex+i].fileName + "' /></li>");
 			}
 		}
 		if(viewerPageIndex < viewerTotal) {
