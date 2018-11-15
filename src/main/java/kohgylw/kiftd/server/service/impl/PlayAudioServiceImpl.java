86cd07604ad2749628a7f63723778a5031adbceb
kohgylw <kohgylw@163.com>
2018-11-15 17:27:41 +0800

    upload to v1.0.8 新增了一些注释

--- src/main/java/kohgylw/kiftd/server/service/impl/PlayAudioServiceImpl.java
+++ src/main/java/kohgylw/kiftd/server/service/impl/PlayAudioServiceImpl.java
@@ -36,6 +36,16 @@ public class PlayAudioServiceImpl implements PlayAudioService
         return null;
     }
     
+    /**
+     * <h2>解析播放音频文件</h2>
+     * <p>根据音频文件的ID查询音频文件节点，以及同级目录下所有音频文件组成播放列表，并返回节点JSON信息，以便发起播放请求。</p>
+     * <pre>
+     * “月隐山河，风敲驼铃，和调成歌远去堪为异乡客……”
+     * </pre>
+     * @author kohgylw
+     * @param request javax.servlet.http.HttpServletRequest 请求对象
+     * @return String 视频节点的JSON字符串
+     */
     public String getAudioInfoListByJson(final HttpServletRequest request) {
         final AudioInfoList ail = this.foundAudios(request);
         if (ail != null) {
