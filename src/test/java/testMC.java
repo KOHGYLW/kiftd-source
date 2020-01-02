fb83775b846962007c98ddf85ac9660d6f956711
Matongsen <admin@sucheasy.com>
2020-01-02 20:56:56 +0800

    实现全局及当前目录的忽略大小写搜索功能
    Signed-off-by: Matongsen <admin@sucheasy.com>

---
+++ src/test/java/testMC.java
@@ -0,0 +1,9 @@
+import kohgylw.kiftd.mc.ConsoleRunner;
+
+public class testMC {
+    public static void main(String[] args) {
+        String[] strs={"-start"};
+        ConsoleRunner.build(strs);
+    }
+}
+
